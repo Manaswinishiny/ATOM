d7c88441337afe6aeba2fe2b04deae28b0e742ab
manaswini surusomoayajula <manaswini46shiny2003@gmail.com>
2021-07-28 18:36:07 +0530

    second commit

--- h.c
+++ h.c
@@ -7,3 +7,4 @@ sfjrefljfprjf'rjeaf['f];
 hishohc;h:hC:h;Hc;hC:__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16n
 cnncncncncncn
 hrfjkrehfrhf;rheqf;.hef;erqbf;rbfa.ewhf hew fkberp;fbqew;yfy yewvf;qer foybgb;ref;rqefvr;yvfyqewrv;f
+gmrelgrg rpg
